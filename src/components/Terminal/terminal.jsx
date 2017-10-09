1e3c79c9777f58824cfa97ec1e23273c5a17a216
Trever Cullen <trevercullen@gmail.com>
2017-10-08 20:33:34 -0400

    add resume

--- src/components/Terminal/terminal.jsx
+++ src/components/Terminal/terminal.jsx
@@ -8,9 +8,10 @@ class Terminal extends React.Component {
   constructor() {
     super();
 
-    this.home = <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>;
+    this.home = <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>;
     this.chev = <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>;
     this.chevs = <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-chevrons-right"><polyline points="13 17 18 12 13 7"></polyline><polyline points="6 17 11 12 6 7"></polyline></svg>;
+    this.file = <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
     this.user = <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>;
     this.github = <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>;
     this.facebook = <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>;
@@ -37,8 +38,11 @@ class Terminal extends React.Component {
           <p>{this.chev} <span>trev.from</span></p>
           <p className="inset pad">{this.chevs} reno, nv</p>
 
-          <p>{this.chev} <span>trev.edu.school</span></p>
-          <p className="inset pad">{this.chevs} university of michigan</p>
+          <p>{this.chev} <span>trev.edu.schools</span></p>
+          <p className="inset">{this.chevs} university of michigan</p>
+          <p className="inset">{this.chevs} hong kong university of science and technology</p>
+          <p className="inset pad">{this.chevs} instituto mediterráneo sol granada</p>
+
           <p>{this.chev} <span>trev.edu.major</span></p>
           <p className="inset pad">{this.chevs} computer science engineering</p>
           <p>{this.chev} <span>trev.edu.minor</span></p>
@@ -49,6 +53,11 @@ class Terminal extends React.Component {
           <p className="inset">{this.chevs} traveling</p>
           <p className="inset">{this.chevs} motorcycles</p>
           <p className="inset pad">{this.chevs} edm</p>
+
+          <p>{this.chev} <span>trev.résumé</span></p>
+          <p className="inset pad">{this.chevs} 
+            <a href="trever-cullen-resume.pdf" target="_blank">{this.file} trever-cullen-resume.pdf</a>
+          </p>
 
           <p>{this.chev} <span>trev.media</span></p>
           <p className="inset">{this.chevs} 
